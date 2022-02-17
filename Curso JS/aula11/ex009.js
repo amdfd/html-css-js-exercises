9dd83d630ce28853f091868408903ae2255f9989
amdfd <65048587+amdfd@users.noreply.github.com>
2022-02-17 15:31:06 -0300

    Exercises
    Added exercises I've done

---
+++ Curso JS/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'França'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Brasil') {
+  console.log('Você é Brasileiro!')
+} else { // Condição composta
+  console.log('Você é Estrangeiro!')
+}
